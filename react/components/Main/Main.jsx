7549cb2fc06776f5527ad244b9f0a6c4eee2a031
Justin Leach <leachjustin18@yahoo.com>
2016-04-19 16:03:22 -0500

    Basic form. Included normalize. Added button

--- react/components/Main/Main.jsx
+++ react/components/Main/Main.jsx
@@ -4,11 +4,13 @@
 import React from 'react';
 import Divider from 'material-ui/lib/divider';
 import TextField from 'material-ui/lib/text-field';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 export default class Main extends React.Component {
     render() {
         return (
             <main>
+                <form action="">
                 <TextField hintText="First name" underlineShow={false} fullWidth={true}/>
                 <Divider/>
                 <TextField hintText="Last name" underlineShow={false} fullWidth={true}/>
@@ -17,6 +19,8 @@ export default class Main extends React.Component {
                 <Divider/>
                 <TextField hintText="Email" type="email" underlineShow={false} fullWidth={true}/>
                 <Divider/>
+                    <RaisedButton label="Submit" secondary={true}  type="submit"/>
+                </form>
             </main>
         )
     }
